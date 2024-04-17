4a20ea47442227dffb7f1ccc152a567affe29df9
pablocozzi <pablocozzi1@gmail.com>
2024-04-16 23:58:52 -0300

    Agregando clase Nodo

---
+++ clsNodo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstructuraDatos.pry
+{
+    internal class clsNodo
+    {
+        private Int32 cod;
+        private String nom;
+        private String tra;
+        private clsNodo sig;
+
+        public Int32 Codigo
+        { get { return cod; } set { cod = value; } }
+        public String Nombre
+        { get { return nom; } set { nom = value; } }
+        public String Tramite
+        { get { return tra; } set { tra = value; } }
+        public clsNodo Siguiente
+        { get { return sig; } set { sig = value; } }
+
+        public clsNodo Primero;
+        public clsNodo Ultimo;
+
+        public void Agregar (clsNodo Nuevo)
+        {
+            if (Primero == null)
+            {
+                Primero = Nuevo;
+                Ultimo = Nuevo;
+            }
+            else
+            {
+                Ultimo.Siguiente = Nuevo;
+                Ultimo = Nuevo;
+            }
+        }
+
+        public void Eliminar ()
+        {
+            if (Primero == Ultimo)
+            {
+                Primero = null;
+                Ultimo = null;
+            }
+            else
+            {
+                Primero = Primero.Siguiente;
+            }
+        }
+
+        public void Recorrer(DataGridView Grilla)
+        {
+            clsNodo aux = Primero;
+            Grilla.Rows.Clear();
+            while (aux != null)
+            {
+                Grilla.Rows.Add(aux.Codigo, aux.Nombre, aux.Tramite);
+                aux = aux.Siguiente;
+            }
+        }
+        public void Recorrer(ListBox Lista)
+        {
+            clsNodo aux = Primero;
+            Lista.Items.Clear();
+            while (aux != null)
+            {
+                Lista.Items.Add(aux.Codigo + " " + aux.Nombre + " " + aux.Tramite);
+                aux = aux.Siguiente;
+            }
+        }
+        public void Recorrer(ComboBox Combo)
+        {
+            clsNodo aux = Primero;
+            Combo.Items.Clear();
+            while (aux != null)
+            {
+                Combo.Items.Add(aux.Nombre);
+                aux = aux.Siguiente;
+
+            }
+        }
+
+
+
+    }
+}
